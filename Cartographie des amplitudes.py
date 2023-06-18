00da18ff99dd6e3148230914e356a3fea6b82ece
SimonCathala <133324260+SimonCathala@users.noreply.github.com>
2023-06-18 12:40:16 +0200

    Add files via upload

--- Cartographie des amplitudes.py
+++ Cartographie des amplitudes.py
@@ -4,11 +4,13 @@ for i in range (16):
     for j in range (16):
         a=(i*0.5)**2
         b=(j*0.5)**2
-        c=sqrt(a+b)
-        print ("Distance entre le micro 1 et la case (", i,",", j,"):",c)
-        if c != 0 :
-            d=1/(c**2)
-            print ("Amplitude du micro 1 pour la case (", i,",", j,"):",d)
+        dist=sqrt(a+b)
+        print ("Distance entre le micro 1 et la case (", i,",", j,"):",dist)
+        if dist != 0 :
+            ampli=1/(dist**2)
+            print ("Amplitude du micro 1 pour la case (", i,",", j,"):",ampli)
+        else :
+            print ("Amplitude du micro 1 pour la case (", i,",", j,"): 0.0")
 
 
 
@@ -19,11 +21,14 @@ for i in range (16):
         y= abs(j-15)
         a=(i*0.5)**2
         b=(y*0.5)**2
-        c=sqrt(a+b)
-        print ("Distance entre le micro 2 et la case (", i,",", j,"):",c)
-        if c != 0 :
-            d=1/(c**2)
-            print ("Amplitude du micro 2 pour la case (", i,",", j,"):",d)
+        dist=sqrt(a+b)
+        print ("Distance entre le micro 2 et la case (", i,",", j,"):",dist)
+        if dist != 0 :
+            ampli=1/(dist**2)
+            print ("Amplitude du micro 2 pour la case (", i,",", j,"):",ampli)
+        else :
+            print ("Amplitude du micro 2 pour la case (", i,",", j,"): 0.0")
+            
 
 
 
@@ -35,9 +40,11 @@ for i in range (16):
         y= abs(j-15)
         a=(x*0.5)**2
         b=(y*0.5)**2
-        c=sqrt(a+b)
-        print ("Distance entre le micro 2 et la case (", i,",", j,"):",c)
-        if c != 0 :
-            d=1/(c**2)
-            print ("Amplitude du micro 2 pour la case (", i,",", j,"):",d)
+        dist=sqrt(a+b)
+        print ("Distance entre le micro 3 et la case (", i,",", j,"):",dist)
+        if dist != 0 :
+            ampli=1/(dist**2)
+            print ("Amplitude du micro 3 pour la case (", i,",", j,"):",ampli)
+        else :
+            print ("Amplitude du micro 3 pour la case (", i,",", j,"): 0.0")
             
