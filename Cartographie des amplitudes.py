dd72d975570098b69ae2b4f044fe8f239d142725
SimonCathala <133324260+SimonCathala@users.noreply.github.com>
2023-06-19 11:32:48 +0200

    Add files via upload

--- Cartographie des amplitudes.py
+++ Cartographie des amplitudes.py
@@ -1,39 +1,50 @@
 from math import *
-print ("----------------------------------------------------------------------------------------------------------Micro 1----------------------------------------------------------------------------------------------------------")
+
+# Création des dictionnaires pour stocker les amplitudes
+ampli_micro1 = {}
+ampli_micro2 = {}
+ampli_micro3 = {}
+
+# Choisissez combien de chiffres après la virgule vous souhaitez conserver
+num_decimal_places = 15
+
+# Constante pour le coefficient 
+const = 8.854187*10**-12
+
+# Calcul des amplitudes pour le micro 1
 for i in range (16):
     for j in range (16):
         a=(i*0.5)**2
         b=(j*0.5)**2
         dist=sqrt(a+b)
-        print ("Distance entre le micro 1 et la case (", i,",", j,"):",dist)
         if dist != 0 :
-            ampli=1/(dist**2)
-            print ("Amplitude du micro 1 pour la case (", i,",", j,"):",ampli)
+            ampli=const/(dist**2)
         else :
-            print ("Amplitude du micro 1 pour la case (", i,",", j,"): 0.0")
+            ampli = 0.0
+        # Conversion de l'amplitude en entier long en déplaçant la virgule décimale
+        ampli_long = int(ampli * (10 ** num_decimal_places))
+        # Stockage de l'amplitude en format binaire dans le dictionnaire
+        ampli_micro1[(i, j)] = format(ampli_long, 'b')
 
 
-
-
-print ("----------------------------------------------------------------------------------------------------------Micro 2----------------------------------------------------------------------------------------------------------")
+# Calcul des amplitudes pour le micro 2
 for i in range (16):
     for j in range (16):
         y= abs(j-15)
         a=(i*0.5)**2
         b=(y*0.5)**2
         dist=sqrt(a+b)
-        print ("Distance entre le micro 2 et la case (", i,",", j,"):",dist)
         if dist != 0 :
-            ampli=1/(dist**2)
-            print ("Amplitude du micro 2 pour la case (", i,",", j,"):",ampli)
+            ampli=const/(dist**2)
         else :
-            print ("Amplitude du micro 2 pour la case (", i,",", j,"): 0.0")
-            
+            ampli = 0.0
+        # Conversion de l'amplitude en entier long en déplaçant la virgule décimale
+        ampli_long = int(ampli * (10 ** num_decimal_places))
+        # Stockage de l'amplitude en format binaire dans le dictionnaire
+        ampli_micro2[(i, j)] = format(ampli_long, 'b')
 
 
-
-
-print ("----------------------------------------------------------------------------------------------------------Micro 3----------------------------------------------------------------------------------------------------------")
+# Calcul des amplitudes pour le micro 3
 for i in range (16):
     for j in range (16):
         x= abs(i-15)
@@ -41,10 +52,16 @@ for i in range (16):
         a=(x*0.5)**2
         b=(y*0.5)**2
         dist=sqrt(a+b)
-        print ("Distance entre le micro 3 et la case (", i,",", j,"):",dist)
         if dist != 0 :
-            ampli=1/(dist**2)
-            print ("Amplitude du micro 3 pour la case (", i,",", j,"):",ampli)
+            ampli=const/(dist**2)
         else :
-            print ("Amplitude du micro 3 pour la case (", i,",", j,"): 0.0")
-            
+            ampli = 0.0
+        # Conversion de l'amplitude en entier long en déplaçant la virgule décimale
+        ampli_long = int(ampli * (10 ** num_decimal_places))
+        # Stockage de l'amplitude en format binaire dans le dictionnaire
+        ampli_micro3[(i, j)] = format(ampli_long, 'b')
+
+# Imprimer les amplitudes stockées pour vérification
+print(ampli_micro1)
+print(ampli_micro2)
+print(ampli_micro3)
