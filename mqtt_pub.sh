f389a6d1fe90cb8952afe7a5b743860290418196
sylviogspt <sylvio.gasparotto.iut@gmail.com>
2023-06-21 20:45:16 +0200

    mqqt pub de la case

--- mqtt_pub.sh
+++ mqtt_pub.sh
@@ -1,7 +1,8 @@
 a1=$1
 a2=$2
 a3=$3
+a4=$4
 
 
-mosquitto_pub -h 192.168.1.15 -u g34 -t amplitude -m "{\"ampli1\" : $a1 , \"ampli2\" : $a2 , \"ampli3\" : $a3}"
+mosquitto_pub -h 192.168.1.15 -u g34 -t amplitude -m "{\"ampli1\" : $a1 , \"ampli2\" : $a2 , \"ampli3\" : $a3 , \"case\" : $a4}"
 
