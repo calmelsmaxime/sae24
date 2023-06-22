d1be4906f28728322efcfd91cd176fb0c2fd918c
sylviogspt <133120731+sylviogspt@users.noreply.github.com>
2023-06-22 09:38:31 +0200

    Update mqtt_pub.sh

--- mqtt_pub.sh
+++ mqtt_pub.sh
@@ -2,7 +2,9 @@ a1=$1
 a2=$2
 a3=$3
 a4=$4
+a5=$5
 
 
-mosquitto_pub -h 192.168.1.15 -u g34 -t amplitude -m "{\"ampli1\" : $a1 , \"ampli2\" : $a2 , \"ampli3\" : $a3 , \"case\" : $a4}"
+
+mosquitto_pub -h 192.168.102.136 -u g34 -t amplitude -m "{\"ampli1\" : $a1 , \"ampli2\" : $a2 , \"ampli3\" : $a3 , \"case\" : $a4, $5}"
 
